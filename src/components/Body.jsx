16b1d47bb22287a0b67b3b1c39b0d92747e04545
Aniket <39321546+an-k45@users.noreply.github.com>
2019-07-09 16:30:21 -0400

    Template regular and addable hexes in Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { HexGrid, Layout, Hexagon, GridGenerator } from 'react-hexgrid';
+import { HexGrid, Layout, Hexagon, GridGenerator, Text } from 'react-hexgrid';
 
-function Body() {
-  const hexagons = GridGenerator.parallelogram(-2, 3, -2, 1);
+class Body extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      regularHexes: [],
+      addableHexes: []
+    };
+  }
 
-  return (
-    <div className="Body">
-      <h1>Basic example of HexGrid usage.</h1>
-      <HexGrid width={1200} height={1000}>
-        <Layout size={{ x: 7, y: 7 }}>
-          { hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
-        </Layout>
-      </HexGrid>
-    </div>
-  );
+  handleRegularClick() {
+    console.log("You clicked a regular hex!");
+  }
+
+  handleAddableClick() {
+    console.log("You clicked an addable hex!");
+  }
+
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <HexGrid>
+          <Layout>
+            <Hexagon q={0} r={0} s={0} onClick={this.handleRegularClick} />
+            <Hexagon q={0} r={-1} s={1} onClick={this.handleAddableClick} />
+            <Hexagon q={1} r={-1} s={0} onClick={this.handleAddableClick} />
+            <Hexagon q={1} r={0} s={-1} onClick={this.handleAddableClick} />
+            <Hexagon q={0} r={1} s={-1} onClick={this.handleAddableClick} />
+            <Hexagon q={-1} r={1} s={0} onClick={this.handleAddableClick} />
+            <Hexagon q={-1} r={0} s={1} onClick={this.handleAddableClick} />
+          </Layout>
+        </HexGrid>
+      </div>
+    );
+  }
 }
 
 export default Body;
